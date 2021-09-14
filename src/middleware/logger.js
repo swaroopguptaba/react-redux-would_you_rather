d8b47caf3096ce82f3bf04f6335bff56c0e8b611
Swaroop Gupta <swaroopgupta.ba@gmail.com>
2021-09-14 09:14:42 -0400

    adding middleware

---
+++ src/middleware/logger.js
@@ -0,0 +1,11 @@
+//currying pattern of middleware
+const logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.log("The action:", action);
+  const returnValue = next(action);
+  console.log("The new state: ", store.getState());
+  console.groupEnd();
+  return returnValue;
+};
+
+export default logger;
